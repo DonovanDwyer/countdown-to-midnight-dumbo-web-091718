2b059b7bffdc64cc584d51627a5cc3c538c33d85
Donovan Dwyer <dwyer.donovan@gmail.com>
2018-09-08 16:29:01 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -3,10 +3,10 @@
 def countdown(int)
   counter = int
   while counter > 0
-    puts "#{counter} SECOND(S)!"
+    "#{counter} SECOND(S)!"
     counter = counter - 1
   end
-  puts "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
 end
 
 # def countdown_with_sleep(int)
